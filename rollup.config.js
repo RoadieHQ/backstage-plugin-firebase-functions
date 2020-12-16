bcc353afb99be7d43f5ea4bd5b71eb18bb2e57bc
Marek Calus <marek-calus@wp.pl>
2020-12-16 23:58:06 +0100

    Update yarn.lock file

--- rollup.config.js
+++ rollup.config.js
@@ -5,41 +5,40 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      dir: 'dist',
-      entryFileNames: 'index.esm.js',
-      chunkFileNames: 'esm/[name]-[hash].js',
-      format: 'module',
-    },
-    preserveEntrySignatures: 'strict',
-    external: require('module').builtinModules,
-
-    plugins: [
-      peerDepsExternal({
-        includeDependencies: true,
-      }),
-      commonjs({
-        include: ['node_modules/**', '../../node_modules/**'],
-        exclude: ['**/*.stories.*', '**/*.test.*'],
-      }),
-      esbuild({
-        target: 'es2019',
-      }),
-      resolve({
-        mainFields: ['browser', 'module', 'main'],
-      }),
-    ],
+export default [{
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    entryFileNames: 'index.esm.js',
+    chunkFileNames: 'esm/[name]-[hash].js',
+    format: 'module',
   },
+  preserveEntrySignatures: 'strict',
+  external: require('module').builtinModules,
 
-  {
-    input: 'dist/src/index.d.ts',
-    output: {
-      file: 'dist/index.d.ts',
-      format: 'es',
-    },
-    plugins: [dts()],
+  plugins: [
+    peerDepsExternal({
+      includeDependencies: true,
+    }),
+    commonjs({
+      include: ['node_modules/**', '../../node_modules/**'],
+      exclude: ['**/*.stories.*', '**/*.test.*'],
+    }),
+    esbuild({
+      target: 'es2019',
+    }),
+    resolve({
+      mainFields: ['browser', 'module', 'main'],
+    }),
+  ]
+},
+
+{
+  input: 'dist-types/src/index.d.ts',
+  output: {
+    file: 'dist/index.d.ts',
+    format: 'es',
   },
+  plugins: [dts()],
+},
 ];
