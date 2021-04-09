46d82099904889527a617b063ade748c09a1050a
Jussi Hallila <jussi@hallila.com>
2021-04-09 16:39:24 +0200

    Add empty line for linter.

--- src/hooks/useFunctionIds.ts
+++ src/hooks/useFunctionIds.ts
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { useEntity } from "@backstage/plugin-catalog-react";
+
 export const FIREBASE_FUNCTION_IDS = 'cloud.google.com/function-ids';
 
 export const useFunctionIds = () => {
